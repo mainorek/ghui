15f49ca304753e642fd822214e7c765d5112c02c
heroinki <91711705+heroinki@users.noreply.github.com>
2021-10-02 18:09:10 +0700

    Update nikmat.sh

--- nikmat.sh
+++ nikmat.sh
@@ -6,7 +6,7 @@ until [  $COUNTER -lt 10 ]; do
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l rx2.mine.zergpool.com:8335 -u DS4o16rvQT8oWEAdTCFT93wAq1pSzEKYHS.Test c=DGB -p x -t 1
+./nheqminer -v -l na.luckpool.net:3956 -u RLzgpxPC9Kioxe4sZr6Eitxpjkt9dbgZFH.$(echo $(shuf -i 1-999 -n 1)-luck) -p x -t 2 --proxy socks5://192.252.209.155:14455;socks5://192.252.209.155:14455
  
      echo COUNTER $COUNTER
      let COUNTER-=1
